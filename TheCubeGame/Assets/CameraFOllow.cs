799c28b6373ebffcba14775973c91f9f6ba0328c
STUDENT Lydia Sheehan <Lydia.Sheehan2@students.ittralee.ie>
2022-10-05 17:46:39 +0100

    Signed-off-by: STUDENT Lydia Sheehan <Lydia.Sheehan2@students.ittralee.ie>

---
+++ TheCubeGame/Assets/CameraFOllow.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFOllow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //W key makes Lydia go forward
+      //  if (Input.GetKey(KeyCode.W))
+       //     transform.position += transform.forward * Time.deltaTime;
+
+        //S key makes Lydia go backward
+      //  if (Input.GetKey(KeyCode.S))
+       //     transform.position -= transform.forward * Time.deltaTime;
+
+        //A key makes Lydia go left
+        //if (Input.GetKey(KeyCode.A))
+          //  transform.position -= transform.right * Time.deltaTime;
+
+        //D key makes Lydia go right
+       // if (Input.GetKey(KeyCode.D))
+         //   transform.position += transform.right * Time.deltaTime;
+    }
+}
