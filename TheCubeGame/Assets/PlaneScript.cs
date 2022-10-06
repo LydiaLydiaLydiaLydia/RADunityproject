7b2887dc5e36c9f0f96a6bed39b1e73280fb36ac
STUDENT Lydia Sheehan <Lydia.Sheehan2@students.ittralee.ie>
2022-10-06 10:52:56 +0100

    Signed-off-by: STUDENT Lydia Sheehan <Lydia.Sheehan2@students.ittralee.ie>

---
+++ TheCubeGame/Assets/PlaneScript.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneScript : MonoBehaviour
+{
+    //Turns out this is object-oriented coding. Who knew!
+    //apparently i should know the inderlying principles of object-oriented programming already. Cool, love that.
+
+    //making health an integer :) (current/maximum health points)
+    int CHP;
+    public int MHP = 100;
+    //grey variables means they haven't been used
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CHP = MHP
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //we're making a method down the bottom :) this is only convention
+
+    //what the fuck is internal? what's the alternative? 
+    internal void takeDamage(int damage)
+    {
+        //check out this new way to reference a key
+        
+        //if (Input.GetKeyDown("h"))
+         //   takeDamage(12);
+
+        CHP -= damage;
+        print("Ouch that hurt me health! Yowza! Now I only have" + CHP + "health left");
+
+        
+    }
+
+    internal int whatsYourMaxHealth()
+    {
+        return MHP;
+    }
+}
