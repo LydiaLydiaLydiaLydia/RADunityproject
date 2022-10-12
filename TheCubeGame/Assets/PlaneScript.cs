a303f891dfc18db45d70ebacd8b0804f01cc0263
STUDENT Lydia Sheehan <Lydia.Sheehan2@students.ittralee.ie>
2022-10-12 12:26:04 +0100

    Covered instantiation in class; turning an asset into a prefab and making it a public thing. Signed-off-by: STUDENT Lydia Sheehan <Lydia.Sheehan2@students.ittralee.ie>

--- TheCubeGame/Assets/PlaneScript.cs
+++ TheCubeGame/Assets/PlaneScript.cs
@@ -16,7 +16,7 @@ public class PlaneScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CHP = MHP
+        CHP = MHP;
     }
 
     // Update is called once per frame
